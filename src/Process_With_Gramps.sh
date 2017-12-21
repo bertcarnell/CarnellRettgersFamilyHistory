ae01cc965d67454c784f5d09dc4ba186cd8b1bdc
bertcarnell <bertcarnell@gmail.com>
2017-12-21 01:22:21 -0500

    Updated tree and linux and R scripts

--- src/Process_With_Gramps.sh
+++ src/Process_With_Gramps.sh
@@ -3,7 +3,7 @@
 if [ $(id -u) = "0" ]; then
 	echo "superuser used"
 	# ensure the right directory is mapped (mount -t vboxsf [Name in vbox mount] [path in linux]
-	mount -t vboxsf Downloads ~/share/
+	mount -t vboxsf Family_Tree_Maker ~/share/
 fi
 
 if [ $(id -u) != "0" ]; then
@@ -13,7 +13,7 @@ fi
 echo "--------------------"
 
 # Path to GEDCOM
-gedcomFile=Carnell_2017-05-13_01.ged
+gedcomFile=Carnell_2017-12-20.ged
 gedcomPath=~/share
 
 # output path
@@ -31,11 +31,30 @@ Josey=I0023
 
 # Gramps Version and path
 GrampsVersion=.
-GrampsPath=~/Documents/repositories/Gramps
+GrampsPath=~/Documents/repositories/gramps
 GrampsCommand="python3 $GrampsPath/Gramps.py"
 
+# -y Don't ask to confirm dangerous actions
+# -q Suppress progress indication output
+# -i import
+# -a action
+# -p options for the action
+#    use this to get the action options
+#    $GrampsCommand $GrampsOptionsStart "name=det_ancestor_report,show=all"
+#    pageben = page break before endnotes
+#    date_format = 0 (YYY-MM-DD), 1 (MM-DD-YY), 2 (Month Day, Year), 3 (MON DAY, YEAR), 4 (Day Month Year", 5 (DAY MON YEAR)
+#    listc = list children
+#    listc_spouses = list spouses of children
+#    fulldates = use full dates
+#    desref
+#    incnotes = include notes
+#    incattrs = include attributes
+#    incphotos = include photos
+#    off=output format
+#    of = output file
+
 GrampsOptionsStart="-y -q -i $gedcomPath/$gedcomFile -a report -p"
-GrampsOptionsReport="name=det_ancestor_report,listc=True,listc_spouses=True,fulldates=True,desref=True,incnotes=False,incattrs=False,incphotos=False,off=tex,of=$outputPath/tex_input/det_ancestor_report_"
+GrampsOptionsReport="name=det_ancestor_report,pageben=True,date_format=5,listc=True,listc_spouses=True,fulldates=True,desref=True,incnotes=False,incattrs=False,incphotos=False,off=tex,of=$outputPath/tex_input/det_ancestor_report_"
 
 $GrampsCommand $GrampsOptionsStart "$GrampsOptionsReport"Bechtel.tex,pid=$Bechtel,initial_sosa=1000
 $GrampsCommand $GrampsOptionsStart "$GrampsOptionsReport"Connell.tex,pid=$Connell
